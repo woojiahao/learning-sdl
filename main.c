5437e5b26b7f1cec1c6a3a9c777f0f731823e2dd
Jiahao, Woo <woojiahao1234@gmail.com>
2026-02-02 14:27:04 +0800

    Add window loading

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <string.h>
+#include <stdbool.h>
 
 const static int width = 640;
 const static int height = 480;
@@ -15,20 +15,65 @@ void close();
 
 int main()
 {
+  if (init() == false)
+  {
+    SDL_Log("Unable to initialize program\n");
+    return 1;
+  }
+  else
+  {
+    bool quit = false;
+
+    SDL_Event e;
+    SDL_zero(e);
+
+    while (quit == false)
+    {
+      while (SDL_PollEvent(&e) == true)
+      {
+        if (e.type == SDL_EVENT_QUIT)
+        {
+          quit = true;
+        }
+      }
+
+      SDL_FillSurfaceRect(surface, NULL, SDL_MapSurfaceRGB(surface, 0xFF, 0xFF, 0xFF));
+      SDL_UpdateWindowSurface(window);
+    }
+
+    close();
+  }
   return 0;
 }
 
 bool init()
 {
-  bool success = true;
-
   if (SDL_Init(SDL_INIT_VIDEO) == false)
   {
     SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
-    success = false;
-  } else {
-
+    return false;
+  }
+  else
+  {
+    window = SDL_CreateWindow("SDL3 Tutorial: Hello SDL3", width, height, 0);
+    if (window == NULL)
+    {
+      SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+      return false;
+    }
+    else
+    {
+      surface = SDL_GetWindowSurface(window);
+    }
   }
 
-  return success;
+  return true;
+}
+
+void close()
+{
+  SDL_DestroyWindow(window);
+  window = NULL;
+  surface = NULL;
+  SDL_Quit();
 }
